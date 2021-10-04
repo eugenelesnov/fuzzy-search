c172503748c143f76fb120dd22323c9bb45488fd
EugeneLesnov <zheka.lesnov1996@gmail.com>
2021-10-04 22:57:35 +0300

    chore: polish some style issues

--- src/test/java/unit/LevenshteinSearchTest.java
+++ src/test/java/unit/LevenshteinSearchTest.java
@@ -13,51 +13,42 @@ class LevenshteinSearchTest {
 
     @Test
     void shouldThrowNullPointerException() {
-        // given
         String str1 = null;
         String str2 = null;
 
-        // then
         assertThrows(NullPointerException.class, () -> levenshtein(str1, str2));
     }
 
     @Test
     void shouldReturnZero() {
-        // given
         String str1 = "test";
         String str2 = "test";
 
-        // then
         int distance = levenshtein(str1, str2);
         assertEquals(0, distance);
     }
 
     @Test
     void shouldReturnLevenshteinDistance() {
-        // given
         String str1 = "test";
         String str2 = "testtest";
         // expected distance is 4 because difference between
         // str1 and str2 is 4 actions of character deletions
         int expectedDistance = 4;
 
-        // then
         int actualDistance = levenshtein(str1, str2);
         assertEquals(expectedDistance, actualDistance);
     }
 
     @Test
     void shouldPrintResultMap() {
-        // given
         int precision = 4;
         String token = "eugene";
         Collection<String> source = TestUtil.getStringCollection();
 
-        // then
-        Map<String, Integer> matched = levenshteinSearch(precision, token, source, String::toString);
-        assertNotNull(matched);
+        Map<String, Integer> result = levenshteinSearch(precision, token, source, String::toString);
 
-        matched.forEach((k, v) -> System.out.println("Token: " + k + "; Levenshtein distance: " + v));
-
+        assertNotNull(result);
+        result.forEach((k, v) -> System.out.println("Token: " + k + "; Levenshtein distance: " + v));
     }
 }
--- src/test/java/unit/NgramSearchTest.java
+++ src/test/java/unit/NgramSearchTest.java
@@ -17,51 +17,42 @@ class NgramSearchTest {
 
         @Test
         void shouldThrowIllegalStateExceptionWhenEmptySearchTerm() {
-            // given
             int power = 3;
             String token = "";
             Collection<String> source = TestUtil.getStringCollection();
 
-            // then
             assertThrows(IllegalStateException.class, () -> ngramSearch(power, 20f, token, source, String::toString));
         }
 
         @Test
         void shouldReturnEmptyMapWhenEmptySourceCollection() {
-            // given
             int power = 3;
             String token = "token";
             Collection<String> source = new ArrayList<>();
 
-            // then
             Map<String, Float> actualResult = ngramSearch(power, 20f, token, source, String::toString);
             assertTrue(actualResult.isEmpty());
         }
 
         @Test
         void shouldThrowIllegalStateExceptionWhenIncorrectPower() {
-            // given
             int n = -3;
             String token = "term";
             Collection<String> source = TestUtil.getStringCollection();
 
-            // then
             assertThrows(IllegalStateException.class, () -> ngramSearch(n, 20f, token, source, String::toString));
         }
 
         @Test
         void shouldPrintResultMap() {
-            // given
             int power = 2;
             String token = "eugene";
             Collection<String> source = TestUtil.getStringCollection();
 
-            // then
             Map<String, Float> result = ngramSearch(power, 40f, token, source, String::toString);
             assertNotNull(result);
 
             result.forEach(((k, v) -> System.out.println("Token: " + k + "; Match percentage: " + v + "% ;")));
-
         }
     }
 
